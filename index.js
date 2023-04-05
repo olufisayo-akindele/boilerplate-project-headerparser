b995a9b2a4e84929d1b65dea6c4357efd7e00294
olufisayo-akindele <67718162+olufisayo-akindele@users.noreply.github.com>
2023-04-05 17:09:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
-app.used(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -22,6 +22,14 @@ app.get('/', function(req, res) {
 // your first API endpoint...
 app.get('/api/hello', function(req, res) {
     res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', function(req, res) {
+    console.log(req);
+    const ipaddress = req.headers['x-forwarded-for'];
+    const language = req.headers['accept-language'];
+    const software = req.headers['user-agent'];
+    res.json({ ipaddress, language, software });
 });
 
 // listen for requests :)
